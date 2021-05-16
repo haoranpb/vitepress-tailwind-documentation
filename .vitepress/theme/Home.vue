615a8773fc6136417fd42166b2281627b8368ea4
Haoran <ludanxer@icloud.com>
2021-05-16 15:15:36 +0200

    feat: copy to clipboard

--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -59,6 +59,7 @@
 
           <button
             class="w-full sm:w-auto flex-none bg-gray-50 text-gray-400 hover:text-gray-900 font-mono leading-6 py-3 sm:px-6 border border-gray-200 rounded-xl flex items-center justify-center space-x-2 sm:space-x-4 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-300 focus:outline-none transition-colors duration-200"
+            @click="writeClipboard"
           >
             <span class="text-gray-900">
               <span class="hidden sm:inline text-gray-500" aria-hidden="true">
@@ -83,5 +84,10 @@ import OutLink from './components/OutLink.vue'
 
 export default {
   components: { SearchBox, Logo, IconGitHub, IconCopy, OutLink },
+  methods: {
+    writeClipboard() {
+      navigator.clipboard.writeText('npm install tailwindcss')
+    },
+  },
 }
 </script>
